a8f28ad971c26a0abe1c75db9fcc4f67403fccad
TonyStark2018 <tony.stark@mrodt.com>
2021-01-22 17:55:08 +0800

    coding... Signed-off-by: TonyStark2018 <tony.stark@mrodt.com>

--- components/service-item/service-third.vue
+++ components/service-item/service-third.vue
@@ -3,7 +3,7 @@
 		<scroll-view class="mysc-view" scroll-y>
 			<view class="msd_list">
 				<view class="msd_info" v-for="(item,index) in msd_list" :key='index'>
-					<image class="msd_img" :src="getImgSrc(item.msD_Img)" @tap='_previewImage(item.msD_Img)' lazy-load mode="aspectFill"></image>
+					<image class="msd_img" :src="getImgSrc(item.msD_Img)" @tap='_previewImage(item.msD_Img_Src)' lazy-load mode="aspectFill"></image>
 					<view class="msd_content">
 						<view class="msd_name">{{item.msD_Name}}</view>
 						<view class="msd_brand">{{item.msD_Sn}}</view>
@@ -11,6 +11,7 @@
 					</view>
 				</view>
 			</view>
+			<view class="reach_Bottom" v-if="flag">------ 已加载全部数据 ------</view>
 		</scroll-view>
 	</view>
 </template>
@@ -19,7 +20,10 @@
 	export default {
 		data() {
 			return {
-				msd_list: {}
+				msd_list: {},
+				pageindex: 1,
+				total_count: 0,
+				flag: false,
 			};
 		},
 		mounted() {
@@ -28,12 +32,39 @@
 		methods: {
 			async getPageInfo() {
 				const res = await this.$myRequest({
-					url: "/Api/Get_App_MSD_List"
+					url: "/Api/Get_App_MSD_List",
+					data:{
+						pageindex:1
+					}
 				})
 				this.msd_list = res.data.data;
+				this.total_count = this.msd_list.length;
 				console.log(this.msd_list)
+			},
+			
+			async getMoreList() {
+				const res = await this.$myRequest({
+					url: "/Api/Get_App_MSD_List",
+					data: {
+						pageindex: this.pageindex,
+					}
+				})
+				let list = res.data.data;
+				this.msd_list = [...this.msd_list, ...list];
+				this.total_count = this.msd_list.length;
+			
+				if (this.total_count < 10 * this.pageindex) {
+					this.flag = true;
+				}
 			}
-		}
+		},
+		// 上拉加载更多
+		onReachBottom() {
+			if (this.flag == false) {
+				this.pageindex += 1;
+				this.getMoreList();
+			}
+		},
 	}
 </script>
 
@@ -77,6 +108,13 @@
 				}
 			}
 
+		}
+	
+		.reach_Bottom {
+			margin: $margin-width;
+			font-size: $small-font-size;
+			text-align: center;
+			color: #8390a3;
 		}
 	}
 </style>
